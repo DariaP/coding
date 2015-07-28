13d6df0802b0dadae4d3af17f84629787dafe348
Daria <dashka.kh@gmail.com>
2015-07-28 15:29:49 -0700

    Delete median for listst with odd and even length

--- linkedList/solutions/deleteMedian.js
+++ linkedList/solutions/deleteMedian.js
@@ -1,7 +1,9 @@
 
 function deleteMedian(list, median) {
   var iterator1 = list.nodeIterator(),
-      iterator2 = list.nodeIterator(median);
+      iterator2 = list.nodeIterator(median),
+      lengthBefore = 0,
+      lengthAfter = 0;
 
   if (!iterator1.hasNext()) {
     throw "Empty list";
@@ -10,21 +12,43 @@ function deleteMedian(list, median) {
   iterator2.next();
 
   if (!iterator2.hasNext()) {
-    list.deleteHead();
+    var node1 = iterator1.next();
+    if(node1.next() === median) {
+      // two elements list, second median
+      list.deleteNext(node1);
+    } else if (node1 === median) {
+      // one element list
+      list.deleteHead();
+    } else {
+      throw "Not a median";
+    }
   } else {
     iterator2.next();
 
+    var node1 = null;
     while(iterator2.hasNext()) {
       iterator2.next();
-      iterator1.next()
+      node1 = iterator1.next();
     }
 
     var beforeMedian = iterator1.next();
-    if(beforeMedian.next() !== median) {
+
+    if (beforeMedian === median) {
+      // even length, first median
+      if (node1 === null) {
+        list.deleteHead();
+      } else {
+        list.deleteNext(node1);
+      }
+    } else if (beforeMedian.next() === median) {
+      // or odd length
+      list.deleteNext(beforeMedian);
+    } else if (beforeMedian.next().next() === median) {
+      // even length, second median
+      list.deleteNext(beforeMedian.next());
+    } else {
       throw "Not a median";
     }
-
-    list.deleteNext(beforeMedian);
   }
 }
 
--- linkedList/tests/deleteMedian-tests.js
+++ linkedList/tests/deleteMedian-tests.js
@@ -35,12 +35,84 @@ test('it deletes median from three elements list', function(t) {
   t.end();
 })
 
+test('it deletes second median from two elements list', function(t) {
+  var list = singlyLinkedList(),
+      median = singlyLinkedList.node(3);
+  list.add(1);
+  list.addNode(median);
+  deleteMedian(list, median)
+  t.equals(list.toString(), '1');
+  t.end();
+})
+
+test('it deletes first median from two elements list', function(t) {
+  var list = singlyLinkedList(),
+      median = singlyLinkedList.node(3);
+  list.addNode(median);
+  list.add(1);
+  deleteMedian(list, median)
+  t.equals(list.toString(), '1');
+  t.end();
+})
+
+test('it deletes first median from simple list with even lemgth', function(t) {
+  var list = singlyLinkedList(),
+      median = singlyLinkedList.node(3);
+  list.add(1);
+  list.addNode(median);
+  list.add(4);
+  list.add(5);
+  deleteMedian(list, median)
+  t.equals(list.toString(), '1,4,5');
+  t.end();
+})
+
+test('it deletes second median from simple list with even lemgth', function(t) {
+  var list = singlyLinkedList(),
+      median = singlyLinkedList.node(3);
+  list.add(1);
+  list.add(4);
+  list.addNode(median);
+  list.add(5);
+  deleteMedian(list, median)
+  t.equals(list.toString(), '1,4,5');
+  t.end();
+})
+
 test('it throws an exception if not a median', function(t) {
   function call() {
     var list = singlyLinkedList(),
         median = singlyLinkedList.node(3);
     list.addNode(median);
     list.add(1);
+    list.add(5);
+    deleteMedian(list, median)    
+  }
+  t.throws(call, "Not a median");
+  t.end();
+})
+
+test('it throws an exception if not a median', function(t) {
+  function call() {
+    var list = singlyLinkedList(),
+        median = singlyLinkedList.node(3);
+    list.add(1);
+    list.add(5);
+    list.addNode(median);
+    deleteMedian(list, median)    
+  }
+  t.throws(call, "Not a median");
+  t.end();
+})
+
+test('it throws an exception if not a median', function(t) {
+  function call() {
+    var list = singlyLinkedList(),
+        median = singlyLinkedList.node(3);
+    list.add(1);
+    list.add(5);
+    list.add(6);
+    list.addNode(median);
     list.add(5);
     deleteMedian(list, median)    
   }
