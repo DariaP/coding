3407c94f85d7484b761f716d9228aab70fb78267
Daria <dashka.kh@gmail.com>
2015-08-24 00:10:43 -0700

    dfs refactored

--- treesAndGraphs/solutions/findPath.js
+++ treesAndGraphs/solutions/findPath.js
@@ -1,7 +1,8 @@
 function findPath(graph, node1, node2) {
   var result = null;
 
-  graph.dfsPath(node1, function(node, path) {
+  graph.dfsPath(node1, function(path) {
+  	var node = path[path.length - 1];
     if (node === node2) {
       if (!result) {
         result = path;        
--- utils/graph.js
+++ utils/graph.js
@@ -1,18 +1,16 @@
 function graph(adjList) {
 
-  return {
-    dfs: function(startNode, callback) {
-
+  function dfs(startNode, factory, callback) {
       if(adjList.length === 0) {
         return;
       }
 
-      var visited = [];
-
-      var queue = [startNode];
+      var visited = [],
+          queue = [factory.initElement(startNode)];
 
       while (queue.length !== 0) {
-        var nextNode = queue.shift();
+        var nextElement = queue.shift(),
+            nextNode = factory.getNode(nextElement);
 
         if(visited[nextNode]) {
           continue;
@@ -21,77 +19,79 @@ function graph(adjList) {
         }
 
         if (adjList[nextNode]) {
-          queue = queue.concat(adjList[nextNode]);
+          var newElements = factory.makeNextElement(nextElement);
+          queue = queue.concat(newElements);
         }
-        callback(nextNode);
+        callback(nextElement);
       }
+  }
+
+  function dfsFactory() {
+    return {
+      initElement: function(startNode) {
+        return startNode
+      },
+      getNode: function(node) {
+        return node;
+      },
+      makeNextElement: function(node) {
+        return adjList[node];
+      }
+    }
+  }
+
+  function dfsDepthFactory() {
+    return {
+      initElement: function(startNode) {
+        return {
+          node: startNode,
+          depth: 0
+        }
+      },
+      getNode: function(element) {
+        return element.node;
+      },
+      makeNextElement: function(element) {
+        return adjList[element.node].map(function(neighbor) {
+          return {
+            node: neighbor,
+            depth: element.depth + 1
+          };
+        });
+      }
+    }
+  }
+
+  function dfsPathFactory() {
+    return {
+      initElement: function(startNode) {
+        return [startNode]
+      },
+      getNode: function(path) {
+        return path[path.length - 1];
+      },
+      makeNextElement: function(path) {
+        var node = path[path.length - 1];
+        return adjList[node].map(function(neighbor) {
+          var newPath = path.slice();
+          newPath.push(neighbor);
+          return newPath; 
+        });
+      }
+    }
+  }
+
+  return {
+    dfs: function(startNode, callback) {
+      dfs(startNode, dfsFactory(), callback);
     },
 
     dfsPath: function(startNode, callback) {
-
-      if(adjList.length === 0) {
-        return;
-      }
-
-      var visited = [];
-
-      var queue = [[startNode]];
-
-      while (queue.length !== 0) {
-        var nextPath = queue.shift();
-        var nextNode = nextPath[nextPath.length - 1];
-
-        if(visited[nextNode]) {
-          continue;
-        } else {
-          visited[nextNode] = true;
-        }
-
-        if (adjList[nextNode]) {
-          var newPaths = adjList[nextNode].map(function(node) {
-            var newPath = nextPath.slice();
-            newPath.push(node);
-            return newPath;
-          });
-          queue = queue.concat(newPaths);
-        }
-        callback(nextNode, nextPath);
-      }
+      dfs(startNode, dfsPathFactory(), callback);
     },
 
     dfsDepth: function(startNode, callback) {
-
-      if(adjList.length === 0) {
-        return;
-      }
-
-      var visited = [];
-
-      var queue = [{
-        node: startNode,
-        depth: 0
-      }];
-
-      while (queue.length !== 0) {
-        var nextNode = queue.shift();
-
-        if(visited[nextNode.node]) {
-          continue;
-        } else {
-          visited[nextNode.node] = true;
-        }
-
-        if (adjList[nextNode.node]) {
-          var newNodes = adjList[nextNode.node].map(function(node) {
-            return {
-              node: node,
-              depth: nextNode.depth + 1
-            };
-          });
-          queue = queue.concat(newNodes);
-        }
-        callback(nextNode, nextNode);
-      }
+      dfs(startNode, dfsDepthFactory(), callback);
     }
   }
 }
