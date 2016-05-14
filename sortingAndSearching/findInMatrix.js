f078fc52e66e8ea07ced0c2a8a6ad83f7548f6b8
Daria <dashka.kh@gmail.com>
2016-05-14 12:39:12 -0700

    stack instead of recursion

--- sortingAndSearching/findInMatrix.js
+++ sortingAndSearching/findInMatrix.js
@@ -1,92 +1,106 @@
 function find(matrix, value) {
-	var x = matrix.length - 1,
-		y = matrix[0].length - 1;
-	return findHelper(matrix, {x:0,y:0}, {x:x, y:y}, value);
-}
 
-function findHelper(matrix, start, end, value) {
+	var intervals = [{
+		start: {
+			x: 0,
+			y: 0
+		},
+		end: {
+			x: matrix.length - 1,
+			y: matrix[0].length - 1
+		}
+	}];
 
-	if(start.x > end.x || start.y > end.y) {
-		return null;
-	}
+	while (intervals.length !== 0) {
+		var interval = intervals.pop(),
+			start = interval.start,
+			end = interval.end;
 
-
-	var middle = {
-		x: Math.floor((start.x + end.x) / 2),
-		y: Math.floor((start.y + end.y) / 2)
-	};
-
-	if (matrix[middle.x][middle.y] === value) {
-		return middle;
-	} else {
-
-		var res;
-		if (matrix[middle.x][middle.y] < value) {
-			res = findHelper(matrix, {
-				x: middle.x + 1, 
-				y: middle.y + 1},
-				end, value);
-			if (res) {
-				return res;
-			}				
-
-			res = findHelper(matrix, {
-				x: middle.x + 1,
-				y: start.y
-			}, {
-				x: end.x,
-				y: middle.y
-			}, value);
-			if (res) {
-				return res;
-			}
-
-			res = findHelper(matrix, {
-				x: start.x,
-				y: middle.y + 1
-			}, {
-				x: middle.x,
-				y: end.y
-			}, value);
-			if (res) {
-				return res;
-			}
-
-		} else {
-			res = findHelper(matrix, start, {
-				x: middle.x - 1,
-				y: middle.y - 1},
-			 	value);
-			if (res) {
-				return res;
-			}
-
-			res = findHelper(matrix, {
-				x: middle.x,
-				y: start.y
-			}, {
-				x: end.x,
-				y: middle.y - 1
-			}, value);
-			if (res) {
-				return res;
-			}
-
-			res = findHelper(matrix, {
-				x: start.x,
-				y: middle.y
-			}, {
-				x: middle.x - 1,
-				y: end.y
-			}, value);
-			if (res) {
-				return res;
-			}
+		if(start.x > end.x || start.y > end.y) {
+			continue;
 		}
 
+		var middle = {
+			x: Math.floor((start.x + end.x) / 2),
+			y: Math.floor((start.y + end.y) / 2)
+		};
 
-		return null;
+		if (matrix[middle.x][middle.y] === value) {
+
+			return middle;
+
+		} else {
+
+			var res;
+			if (matrix[middle.x][middle.y] < value) {
+
+				intervals.push({
+					start: {
+						x: middle.x + 1, 
+						y: middle.y + 1
+					},
+					end: end
+				});
+
+				intervals.push({
+					start: {
+						x: middle.x + 1,
+						y: start.y
+					},
+					end: {
+						x: end.x,
+						y: middle.y						
+					}
+				});
+
+				intervals.push({
+					start: {
+						x: start.x,
+						y: middle.y + 1
+					},
+					end: {
+						x: middle.x,
+						y: end.y
+					}
+				});
+
+			} else {
+
+				intervals.push({
+					start: start,
+					end: {
+						x: middle.x - 1,
+						y: middle.y - 1						
+					}
+				});
+
+				intervals.push({
+					start: {
+						x: start.x,
+						y: middle.y
+					},
+					end: {
+						x: middle.x - 1,
+						y: end.y						
+					}
+				});
+
+				intervals.push({
+					start: {
+						x: middle.x,
+						y: start.y
+					},
+					end: {
+						x: end.x,
+						y: middle.y - 1
+					}
+				});
+			}
+
+		}
 	}
+
+	return null;
 }
 
 module.exports = find;
